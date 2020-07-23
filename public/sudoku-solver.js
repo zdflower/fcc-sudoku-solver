fffccc952f3567087f810a4bf9c2f12f3d37dc65
zdflower <zdflower@users.noreply.github.com>
2020-07-23 15:18:00 -0300

    Escribí los dos primeros tests de la función que chequea si el input es un número entre 1 y 9.

--- public/sudoku-solver.js
+++ public/sudoku-solver.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   textArea.value = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
 });
 
-/* 
+/*
   Export your functions for testing in Node.
   Note: The `try` block is to prevent errors on
   the client side
@@ -14,5 +14,20 @@ document.addEventListener('DOMContentLoaded', () => {
 try {
   module.exports = {
 
+    isNumberBetweenOneAndNine: (cellContent) => {
+      // cellContent is String, ideally, a one character length string representing a number betwen 1 and 9.
+      // Returns true if cellContent can be casted to a number and that number is in [1 .. 9] range
+      // Returns false otherwise.
+      // Reflections:
+      // Maybe it should check that the number is integer? That depends on cellContent, if it is just a character, then the number can't have decimal part other than 0.
+
+      try {
+        const number = Number(cellContent);
+        return number > 0 && number < 10;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    },
   }
 } catch (e) {}
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -24,33 +24,37 @@ suite('UnitTests', () => {
         Solver = require('../public/sudoku-solver.js');
       });
   });
-  
+
   // Only the digits 1-9 are accepted
   // as valid input for the puzzle grid
-  suite('Function ____()', () => {
+  suite('Function isNumberBetweenOneAndNine?(cellContent)', () => {
     test('Valid "1-9" characters', (done) => {
       const input = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-      // done();
+      input.forEach((item) => {
+        assert.isOk(Solver.isNumberBetweenOneAndNine(item));
+      });
+      done();
     });
 
-    // Invalid characters or numbers are not accepted 
+    // Invalid characters or numbers are not accepted
     // as valid input for the puzzle grid
     test('Invalid characters (anything other than "1-9") are not accepted', (done) => {
       const input = ['!', 'a', '/', '+', '-', '0', '10', 0, '.'];
-
-      // done();
+      input.forEach((item) => {
+        assert.isNotOk(Solver.isNumberBetweenOneAndNine(item));
+      });
+      done();
     });
   });
-  
+
   suite('Function ____()', () => {
     test('Parses a valid puzzle string into an object', done => {
       const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      
+
       // done();
     });
-    
-    // Puzzles that are not 81 numbers/periods long show the message 
+
+    // Puzzles that are not 81 numbers/periods long show the message
     // "Error: Expected puzzle to be 81 characters long." in the
     // `div` with the id "error-msg"
     test('Shows an error for puzzles that are not 81 numbers long', done => {
@@ -58,7 +62,7 @@ suite('UnitTests', () => {
       const longStr = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6...';
       const errorMsg = 'Error: Expected puzzle to be 81 characters long.';
       const errorDiv = document.getElementById('error-msg');
-      
+
       // done();
     });
   });
@@ -78,13 +82,13 @@ suite('UnitTests', () => {
       // done();
     });
   });
-  
-  
+
+
   suite('Function ____()', () => {
     // Returns the expected solution for a valid, incomplete puzzle
     test('Returns the expected solution for an incomplete puzzle', done => {
       const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      
+
       // done();
     });
   });
