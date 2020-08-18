6a0e0fe81b09a4d1daeb4a5fe3c6c22626590cdb
zdflower <zdflower@users.noreply.github.com>
2020-08-18 12:44:25 -0300

    Escribí el test para el botón de clear puzzle. Escribí una parte del test del botón solve puzzle.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -42,26 +42,73 @@ suite('Functional Tests', () => {
       const textArea = document.getElementById("text-input");
       const celdasGrillaTablero = document.getElementsByClassName("sudoku-input");
       const clearBtn = document.getElementById("clear-button");
-      // simular evento de click
-      // https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Creating_and_triggering_events
-      // si en algún momento entiendo cómo simular el click del botón clear, el test sería algo así:
-      // click
+      //simulateClick(clearBtn);
+      //llamo al manejador directamente, ya que con simulateClick me da error: MouseEvent is not defined.
+      Solver.cleanBoard();
       // assert: textArea.value o textContent o como se llame donde va el texto del textarea === ""
+      assert.equal(esVacia(textArea.value), true);
       // assert: para toda celda de celdasGrillaTablero, el contenido de la celda va a ser ""
-      // done();
+      assert.equal(todas(celdasGrillaTablero, esVacia), true);
+      done();
     });
     
     // Pressing the "Solve" button solves the puzzle and
     // fills in the grid with the solution
     test('Function showSolution(solve(input))', done => {
       const solveBtn = document.getElementById("solve-button");
+      // habría que poner un puzzle para resolver
+      const textArea = document.getElementById("text-input");
+      textArea.value = '.7.89.....5....3.4.2..4..1.5689..472...6.....1.7.5.63873.1.2.8.6..47.1..2.9.387.6'; // es un puzzle con solución en la biblioteca de puzzles.
       // algo similar al test de clearInput pero se va a partir de ingresar un puzzle en text area, clickear el boton solve, entonces vamos a hacer: 
-      // assert: el contenido de textarea va a ser la solución
+      Solver.solveSudokuHandler(); //esto va a buscar una solución y la va a poner en texArea.value reemplazando el puzzle incompleto.
+      const solution = '473891265851726394926345817568913472342687951197254638734162589685479123219538746';
+      assert.equal(textArea.value, solution);
+      // COMPLETAR: ---v
       // assert: el contenido de cada celda va a corresponder con cada número de la solución.
-      // done();
+      done();
+    });
+  });
+  
+  suite('Helpers para los tests', () => {
+    test('Function esVacía()', done => {
+      const str = "";
+      const str2 = "Esta no es una cadena vacia.";
+      assert.equal(esVacia(str), true);
+      assert.equal(esVacia(str2), false);
+      done();
+    });
+    
+    test('Function todas()', done => {
+    // la testeo con un array, aunque en el programa la uso con una HTMLCollection.
+      const celdasArrayNoTodasVacias = [{"value": "."}, {"value": "4"}, {"value": ""}];
+      const celdasArray = [{"value": ""}, {"value": ""}, {"value": ""}, {"value": ""}];
+      assert.equal(todas(celdasArrayNoTodasVacias, esVacia), false, "No están todas vacías, debería dar false");
+      assert.equal(todas(celdasArray, esVacia), true, "Están todas vacías, debería dar true.");
+      done();
     });
   });
 });
 
 
+/* https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Creating_and_triggering_events, modificado */
 
+function simulateClick(btn) {
+  const event = new MouseEvent('click', {view: window, bubbles: true, cancelable: true});
+  btn.dispatchEvent(event);
+}
+
+// celdas es lo que te devuelve getElementsByClassName, que es como un array pero no lo es. Se llama HTMLCollection.
+// me gustaría como segundo parámetro un predicado que es el que se aplicaría a cada celda para ver si lo cumple.
+function todas(celdas, fn_str){
+  let result = true;
+  for (let i= 0; i < celdas.length; i++){
+    const celda = celdas[i];
+    result = result && fn_str(celda.value);
+  }
+  return result;
+}
+
+// ¿Cómo conviene chequearlo? ¿Por longitud igual a 0 o por igualdad con ""?
+function esVacia(str){
+  return str.length === 0;
+}
