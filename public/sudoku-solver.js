fc7558e6c5e7a2e25b9e46add463366466348f13
zdflower <zdflower@users.noreply.github.com>
2020-08-21 15:41:48 -0300

    Importé los puzzles y soluciones de puzzle-strings.js

--- public/sudoku-solver.js
+++ public/sudoku-solver.js
@@ -2,6 +2,7 @@
 - Revisar código duplicado o redundante.
 - Revisar código no utilizado e innecesario. 
 */
+import { puzzlesAndSolutions as LIBRARY_OF_SOLUTIONS } from './puzzle-strings.js';
 
 const LETRAS_FILAS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
 const INPUT_LENGTH_ERROR_MSG = 'Error: Expected puzzle to be 81 characters long.';
@@ -21,28 +22,6 @@ const INICIO_FIN_BLOQUES = [ [[0,0], [2,2]],
 	                     [[6,6], [8,8]]
                            ]; // el primer par de cada subarray contiene fila y columna de la celda inicial del bloque, arriba a la izquierda
                               // el segundo par contiene fila y columna de la celda final, abajo a la derecha.
-const LIBRARY_OF_SOLUTIONS = [
-  [
-    '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.',
-    '135762984946381257728459613694517832812936745357824196473298561581673429269145378'
-  ],
-  [
-    '5..91372.3...8.5.9.9.25..8.68.47.23...95..46.7.4.....5.2.......4..8916..85.72...3',
-    '568913724342687519197254386685479231219538467734162895926345178473891652851726943'
-  ],
-  [
-    '..839.7.575.....964..1.......16.29846.9.312.7..754.....62..5.78.8...3.2...492...1',
-    '218396745753284196496157832531672984649831257827549613962415378185763429374928561'
-  ],
-  [
-    '.7.89.....5....3.4.2..4..1.5689..472...6.....1.7.5.63873.1.2.8.6..47.1..2.9.387.6',
-    '473891265851726394926345817568913472342687951197254638734162589685479123219538746'
-  ],
-  [
-    '82..4..6...16..89...98315.749.157.............53..4...96.415..81..7632..3...28.51',
-    '827549163531672894649831527496157382218396475753284916962415738185763249374928651'
-  ]
-];
 
 const textInput = document.getElementById('text-input');
 
